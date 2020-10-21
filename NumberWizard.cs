d177aa91042f4684dcd3696894e37d25f7f8f1d4
JayListen <71913081+JayListen@users.noreply.github.com>
2020-10-20 23:08:54 -0400

    Completed Numbers Wizard Code
    This code basically goes over how to use the debug console, how to do basic programming, and is a little number guessing game that works by asking if a number is higher or lower than the guess

---
+++ NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number but don't tell me.");
+        Debug.Log("The Highest number is: " + max);
+        Debug.Log("The lowest number is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Push up if higher, Push Down if lower, Push Enter if correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Looks like I hit the right number :D");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than: " + guess);
+    }
+}
